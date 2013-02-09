50ea1bf666677930091754d0489fac9c40088b6b
Felix C. Stegerman <flx@obfusk.net>
2013-02-09 01:59:55 +0100

    fixes; docs

--- lib/obfusk/data.rb
+++ lib/obfusk/data.rb
@@ -15,29 +15,37 @@ require 'set'
 module Obfusk
   module Data
 
+    # this helper allows us to run field in the block passed to data
     class FieldsHelper                                          # {{{1
+      # init
       def initialize
         @_fields = []
       end
 
+      # add field
       def field (*args)
         @_fields << Obfusk::Data.field(*args)
       end
 
+      # get fields
       def _fields
         @_fields
       end
     end                                                         # }}}1
 
+    # this helper allows us to run data in the block passed to union
     class DatasHelper                                           # {{{1
+      # init
       def initialize
         @_datas = {}
       end
 
+      # add data
       def data (value, &block)
         @_datas[value] = Obfusk::Data.data &block
       end
 
+      # get datas
       def _datas
         @_datas
       end
@@ -45,28 +53,34 @@ module Obfusk
 
     # --
 
+    # empty sequence?
     def self._blank? (x)
       String === x || Enumerable === x ? x.empty? : false
     end
 
+    # if only we had clojure's :keys ;-(
     def self._get_keys (x, keys)
       x.values_at *keys.map(&:to_sym)
     end
 
+    # add error
     def self._error (st, *msg)
       e1 = st.get :errors
       e2 = e1.add msg.join
       st.put :errors, e2
     end
 
+    # run block in instance of helper
+    def self._blk_hlp (cls, block)
+      cls.new.instance_eval &block
+    end
+
     # --
 
-    # A data validator.  ...
-    def self.data (opts = {}, &block)                           # {{{1
+    # ...
+    def self.data_ (fields, opts = {})                          # {{{1
       o_flds  = opts[:other_fields].to_proc
       isa     = opts.fetch :isa, []
-
-      fields  = block ? block[FieldsHelper.new]._fields : []
       st      = Hamster.hash errors: Hamster.vector,
                   processed: Hamster.set
 
@@ -90,8 +104,15 @@ module Obfusk
       }
     end                                                         # }}}1
 
+    # A data validator.  ...
+    def self.data (opts = {}, &block)
+      fields = block ? _blk_hlp(FieldsHelper, block)._fields : []
+      data_ fields, opts
+    end
+
     # --
 
+    # validate a field
     def self._validate_field (name, predicates, opts, x, st)    # {{{1
       field = x[name]
       preds = predicates.map(&:to_proc)
@@ -133,16 +154,20 @@ module Obfusk
 
     # --
 
-    # Union of data fields.  ...
-    def self.union (key, opts = {}, &block)
-      b = block[DatasHelper.new]._datas
+    # ...
+    def self.union_ (key, datas)
       f = field key, [->(x) { Symbol === x }]
 
       ->(x, st ; fields, fields_) {
-        fields  = b.fetch x.fetch(key)
-        fields_ = fields + [f]
-        fields_.reduce(st) { |s, field| field[x, s] }
+        fields = datas.fetch x.fetch(key) + [f]
+        fields.reduce(st) { |s, field| field[x, s] }
       }
+    end
+
+    # Union of data fields.  ...
+    def self.union (key, opts = {}, &block)
+      datas = _blk_hlp(DatasHelper, block)._datas
+      data_ [union_(key, datas)], opts
     end
 
     # --
